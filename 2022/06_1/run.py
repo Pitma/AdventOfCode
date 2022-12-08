8b7c05e5fdc959d706ef9714fd0f6ea04e01e03c
Patrick Mainka <p.mainka@akf.de>
2022-12-08 16:13:35 +0100

    Day 6

---
+++ 2022/06_1/run.py
@@ -0,0 +1,29 @@
+import re
+file = open("input.txt","r")
+file_data = file.read()
+loops = 4
+input =  [*file_data]
+check = []
+
+#print(input)
+for pos in range(0,len(input)):
+
+        fail = 0
+        for c in range(loops):
+            check.extend(input[pos+c])
+        print("POS:",pos,"CHECK",check)
+        if len(list(set(check))) == loops:
+            print("Ergebnis: ",pos+loops)
+            break
+        check.clear()
+
+
+
+
+
+
+
+
+
+
+
