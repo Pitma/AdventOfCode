1a1fa53fd02f020460f24c8ae8d47845b21cb740
Patrick Mainka <p.mainka@akf.de>
2023-12-07 20:12:19 +0100

    day7

--- 2023/day07/day07.py
+++ 2023/day07/day07.py
@@ -2,7 +2,7 @@
 # from itertools import *
 # from math import *
 #from submit_answer import submit_answer
-
+from collections import Counter
 
 
 #      N   E   S   W
@@ -14,8 +14,48 @@ LEVEL = 2
 SAMPLE_ANSWERS = [6440, 5905]
 SAMPLE_ANSWER = SAMPLE_ANSWERS[LEVEL - 1]
 
+def rank(hand, part_2=False):
+    # Special-case JJJJJ; other hands Js just become the most common card.
+    if part_2 and hand != 'JJJJJ':
+        no_jokers = Counter(c for c in hand if c != 'J').most_common()
+        hand = hand.replace('J', no_jokers[0][0])
+
+    cmn = Counter(hand).most_common()
+
+    # Five of a Kind
+    if cmn[0][1] == 5:
+        return 6
+
+    # Four of a Kind
+    elif cmn[0][1] == 4:
+        return 5
+
+    # Full House / Three of a Kind (based on second-most common card)
+    elif cmn[0][1] == 3:
+        return 2 + cmn[1][1]
+
+    # Two Pair / One Pair (based on second-most common card)
+    elif cmn[0][1] == 2:
+        return cmn[1][1]
+
+    return 0
+
+
+def tiebreak(hand, part_2=False):
+    return tuple(card_score(c, part_2) for c in hand)
+
+
+def card_score(card, part_2=False):
+    if part_2:
+        return 'J23456789TQKA'.index(card)
+    else:
+        return '23456789TJQKA'.index(card)
 
 def solve(input_string: str) -> int or str:
+    hands =[]
+    for line in input_string:
+        hand, bid = line.split()
+        hands.append((hand, int(bid)))
     #A = list(input_string)
     # A = list(map(int, input_string.split(',')))
     #A = [line for line in input_string.split(' ')]
@@ -25,94 +65,24 @@ def solve(input_string: str) -> int or str:
     N = len(A)
     print("N =", N)
     #print(A)
-    # Werte aus der Datei extrahieren und verarbeiten
-    data_map = {}
-    #card_values = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
-    if LEVEL ==1:
-        card_values = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
-    else:
-        card_values = ['J','2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A']
-    
-    for line in A:
-        # Die Werte trennen und Leerzeichen entfernen
-        values = line.strip().split()
+    # Solve part 1.
+    part_1 = 0
+    for i, (hand, bid) in enumerate(sorted(hands, key=lambda h: (rank(h[0]), tiebreak(h[0]))), start=1):
+        part_1 += i * bid
+
+    print("Part 1:", part_1)
+
+
+    # Solve part 2.
+    part_2 = 0
+    for i, (hand, bid) in enumerate(sorted(hands, key=lambda h: (rank(h[0], part_2=True), tiebreak(h[0], part_2=True))), start=1):
+        part_2 += i * bid
         
-        # Wenn es mindestens zwei Werte gibt
-        if len(values) >= 2:
-            cards = values[0]  # Der erste Wert wird als "cards" verwendet
-            bid = ' '.join(values[1:])  # Der Rest wird als "bid" verwendet
-            
-           
-
-            # Ein neues Dictionary für die Zeile erstellen und in die Map einfügen
-            row_data = {'cards': cards, 'bid': bid}
-            
-            card_count = {}
-            for card in cards:
-                card_count[card] = card_count.get(card, 0) + 1
-            row_data['card_count'] = card_count
-
-        # Überprüfen, ob es ein Full House ist und Punkte vergeben
-        points = 0
-        #if 5 in card_count.values() :
-            #points = 7 
-        if 4 in card_count.values() :
-            points = 6
-        elif 3 in card_count.values() and 2 in card_count.values():
-             points = 5  
-             if int(card_count.get('J') or 0) > 0:
-                points +=1
-                #print("ja hat Joker",values)
-            
-        elif 3 in card_count.values() and 2 not in card_count.values():
-            points = 4  
-            if int(card_count.get('J') or 0) > 1:
-                points +=2
-            elif int(card_count.get('J') or 0) == 1:
-                points +=1
-                #print("ja hat Joker",values)
-        elif list(card_count.values()).count(2) == 2:
-            points = 3
-            #print(card_count.get('J'))
-            if int(card_count.get('J') or 0) > 1 :
-                    points +=3
-                    #print("ja hat Joker",values)
-
-        elif list(card_count.values()).count(2) == 1:
-            points = 2
-            if int(card_count.get('J') or 0) == 1:
-                points +=2
-                #print("ja hat Joker",values)    
-        else:
-            points = 1
-            if card_count.get('J') == 1:
-                points +=1
-                #print("ja hat Joker",values)    
-
-        row_data['points'] = points
-        data_map[values[0]] = row_data
-    # Die Liste nach Punkten sortieren
-    sorted_list = sorted(data_map.items(), key=lambda x: (x[1]['points'], [card_values.index(card) for card in x[1]['cards']]), reverse=True)
-
-    #print(sorted_list)
-    count=len(sorted_list)
-    total=0
-    for row_index,card in sorted_list:
-        #print("index",row_index)
-        total += count * int(card.get('bid'))
-        count -= 1
-        
-    # M = len(A[0])
-    # 248465995 too high
-    # for i in range(N):
-
-
-    # for i in range(N):
 
     if LEVEL == 1:
-        return total
+        return part_1
     else:
-        return total
+        return part_2
 
 
 def main():
