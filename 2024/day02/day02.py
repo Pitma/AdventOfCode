d14066c97e2084fcf5a02379daa2aef6a3a1783d
Patrick Mainka <p.mainka@akf.de>
2024-12-02 10:51:16 +0100

    day02 refactor

--- 2024/day02/day02.py
+++ 2024/day02/day02.py
@@ -33,77 +33,52 @@ class AoCSolver:
         # return [[c for c in line] for line in input_str.split('\n')]
         return input_str
 
-    def solve_part1(self, data: Any) -> Union[int, str]:
-        safe_cnt = 0
+    def check_list_is_valid(report):
+        isAscending = True
+        isDesending = True
+        isAdjacent = True
 
-        for report in data:
-            isAscending = True
-            isDesending = True
-            isAdjacent = True
-
-            for i in range(len(report)-1):
-                    if report[i] >= report[i+1]:
-                        isAscending = False
-                    if report[i] <= report[i+1]:
-                        isDesending = False
-                    if abs(report[i]-report[i+1]) > 3:
-                        isAdjacent = False
-            if (isDesending or isAscending) and isAdjacent:
-                safe_cnt += 1
-
-        return safe_cnt
-        raise NotImplementedError("Part 1 solution not implemented")
-
-    def solve_part2(self, data: Any) -> Union[int, str]:
-        safe_cnt = 0
-        
-        for report in data:
-            isAscending = True
-            isDesending = True
-            isAdjacent = True
-
-            for i in range(len(report)-1):
+        for i in range(len(report)-1):
                 if report[i] >= report[i+1]:
                     isAscending = False
                 if report[i] <= report[i+1]:
                     isDesending = False
                 if abs(report[i]-report[i+1]) > 3:
                     isAdjacent = False
-                
-            if ((isDesending or isAscending) and isAdjacent):
-                print(f"{report}: Bereits sicher")
+        if (isDesending or isAscending) and isAdjacent:
+            return True
+        
+        return False
+
+    def solve_part1(self, data: Any) -> Union[int, str]:
+        safe_cnt = 0
+
+        for report in data:
+            if AoCSolver.check_list_is_valid(report):
+                safe_cnt += 1
+
+        return safe_cnt
+
+    def solve_part2(self, data: Any) -> Union[int, str]:
+        safe_cnt = 0
+        
+        for report in data:
+            if AoCSolver.check_list_is_valid(report):
                 safe_cnt += 1
                 continue
             
             #If not a safe List try to bumb one value and check again    
-            found_solution = False
             for skipId in range(len(report)):
 
                 temp_list = [report[i] for i in range(len(report)) if i != skipId]
                 
-                isAscending = True
-                isDescending = True
-                isAdjacent = True
-                
-                for i in range(len(temp_list)-1):
-                    if temp_list[i] >= temp_list[i+1]:
-                        isAscending = False
-                    if temp_list[i] <= temp_list[i+1]:
-                        isDescending = False
-                    if abs(temp_list[i] - temp_list[i+1]) > 3:
-                        isAdjacent = False
-                        break
-
-                if (isDescending or isAscending) and isAdjacent:
+                if AoCSolver.check_list_is_valid(temp_list):
                     print(f"{report}: Wird sicher nach Entfernen von {report[skipId]} -> {temp_list}")
-                    found_solution = True
+                    safe_cnt += 1
                     break
 
-            if found_solution:
-                safe_cnt += 1
-
         return safe_cnt
-        raise NotImplementedError("Part 2 solution not implemented")
+
 
     def solve(self, input_str: str) -> Union[int, str]:
         """Main solve method."""
